6390ed3ebb5adefafbe9157feb4f8657524f2aee
Danilo Canivel <d.canivel@gmail.com>
2019-07-22 11:17:28 -0400

    some fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,6 @@
 yum install -y httpd php git
-
 service httpd start
-
 cd
-
-git clone https://github.com/linuxacademy/aws-csa-pro-2019.git
-
-cp ./aws-csa-pro-2019/05_ha_scaling_ft/ELB/* /var/www/html
-
+git clone https://github.com/canivel/awsdemo
+cp ./awsdemo/* /var/www/html
 mv /var/www/html/htaccess /var/www/html/.htaccess
--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html>
-<title> AWS AA-ELB-CF </title>
-<body>
-<h1>
-<center>
-<IMG SRC="awscloud.jpg" ALT="awscloud"><br>
-<?php
-echo $_SERVER['SERVER_ADDR'];
-?>
-</center>
-</h1>
-</body>
+    <title> AWS AA-ELB-CF </title>
+    <body>
+        <h1>
+        <center>
+            <IMG SRC="awscloud.jpg" ALT="awscloud"><br>
+            <?php
+                echo $_SERVER['SERVER_ADDR'];
+            ?>
+        </center>
+        </h1>
+    </body>
 </html>
